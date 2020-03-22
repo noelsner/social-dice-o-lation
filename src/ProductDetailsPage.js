395341ac14847331f8cdfcec41ead38e2e0abd8e
Nick Oelsner <noelsner@gmail.com>
2020-03-22 10:21:05 -0700

    update

--- src/ProductDetailsPage.js
+++ src/ProductDetailsPage.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
 const ProductDetailsPage = ({product}) => {
-  console.log(product);
   return (
     <div>
       <h1> {product.name} Details Page</h1>
-      {/* <p> {product.description} </p> */}
     </div>
   );
 };
