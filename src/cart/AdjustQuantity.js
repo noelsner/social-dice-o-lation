ea0ae403d000e9a844bc9e529fb2bd53b6881a82
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-03-26 16:24:01 -0700

    drop down quantity changes

--- src/cart/AdjustQuantity.js
+++ src/cart/AdjustQuantity.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const AdjustQuantity = ({lineItemQuantity}) => {
+    const [quantity, setQuantity] = useState(0);
+
+    useEffect(()=>{
+        if(lineItemQuantity){
+            setQuantity(lineItemQuantity);
+        };
+    },[]);
 
     return ( 
         <div>
-            <select value = {lineItemQuantity}>
+            <select value = {quantity} onChange = {(ev)=> setQuantity(ev.target.value)}>
                 <option value = '1'>1</option>
                 <option value = '2'>2</option>
                 <option value = '3'>3</option>
