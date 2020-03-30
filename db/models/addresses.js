7da31a63b65e4e6afabe6754d1b3287274f7cf32
Philip Sun <philipsun@philips-mbp.attlocal.net>
2020-03-30 16:57:45 -0700

    Address read route and render for loggedin user

--- db/models/addresses.js
+++ db/models/addresses.js
@@ -4,9 +4,12 @@ const addresses = {
   read: async()=> {
     return (await client.query('SELECT * FROM addresses')).rows;
   },
-  create: async({ userId, address1, address2, city, state, country, zipcode})=> {
+  readOneUser: async(userId)=> {
+    return (await client.query('SELECT * FROM addresses WHERE "userId" = $1',[userId])).rows;
+  },
+  create: async({ userId, address1, address2, city, state, country, zipCode})=> {
     const SQL = `INSERT INTO addresses("userId", address1, address2, city, state, country, "zipCode") values($1, $2, $3, $4, $5, $6, $7) returning *`;
-    return (await client.query(SQL, [userId, address1, address2, city, state, country, zipcode])).rows[0];
+    return (await client.query(SQL, [userId, address1, address2, city, state, country, zipCode])).rows[0];
   },
 };
 
---
+++ src/checkout/ListAddresses.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const ListAddresses = ({userId}) => {
+
+    const [addresses, setAddresses] = useState([]);
+
+    const headers = () => {
+        const token = window.localStorage.getItem('token');
+        return {
+          headers: {
+            authorization: token
+          }
+        };
+      };
+
+    useEffect( ()=> {
+        axios.get(`/api/userAddress/${userId}`, headers())
+        .then(response => setAddresses(response.data))
+    }, [])
+
+    return (
+        <div>
+            <label>Choose Shipping Address</label>
+            <form className="d-flex align-items-center">
+            {
+            addresses.map(address => {
+                console.log(address);
+                return (
+                    <div > 
+                        <div key = {address.id} className = "input-group">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                    <input type="radio" aria-label="Radio button for following text input"></input>
+                                </div>
+                            </div>
+                            <div className = "card-body">
+                                <span>{address.address1} </span><br/>
+                                {address.address2 && <span>{address.address2}</span>} 
+                                <span>{address.city}, {address.state} {address.zipCode} </span> 
+                            </div>
+                        </div>   
+                    </div>
+                );
+            } )
+            }
+            </form>
+        </div>
+    )
+}
+export default ListAddresses;
